e7fcec98eb2a831099dbc380b7308919829405e4
Elliot Smith <elljsmee@gmail.com>
2016-12-04 22:28:17 +0000

    added base files/ folder structure

---
+++ _site/index.php
@@ -0,0 +1,18 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+
+// load kirby
+require(__DIR__ . DS . 'kirby' . DS . 'bootstrap.php');
+
+// check for a custom site.php
+if(file_exists(__DIR__ . DS . 'site.php')) {
+  require(__DIR__ . DS . 'site.php');
+} else {
+  $kirby = kirby();
+}
+
+// render
+echo $kirby->launch();
+?>
+<script async="" src="/browser-sync/browser-sync-client.2.11.1.js"></script>
---
+++ gulpfile.js
@@ -0,0 +1,137 @@
+var gulp = require('gulp');
+var gulpUtil = require('gulp-util');
+var browserSync = require('browser-sync').create();
+var stylus = require('gulp-stylus');
+var prefix = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
+var pug = require('gulp-pug');
+var cleanCss = require('gulp-clean-css');
+var combineMQ = require('gulp-combine-mq');
+var unCss = require('gulp-uncss');
+var koutoSwiss = require( "kouto-swiss" );
+var php  = require('gulp-connect-php');
+
+//defining some commonly used paths
+var basePaths = {
+  'bower': './bower_components/',
+  'dev': './_dev/',
+  'dest': './_site/'
+};
+
+//defining source file paths
+var paths = {
+  styles: {
+    src: basePaths.dev + 'assets/styl/',
+    dest: basePaths.dest + 'assets/css/'
+  },
+  images: {
+    src: basePaths.dev +'assets/images/',
+    dest: basePaths.dest + 'assets/images/'
+  },
+  scripts: {
+    src: basePaths.dev +'assets/scripts/',
+    dest: basePaths.dest +'assets/scripts/'
+  }
+};
+
+var appFiles = {
+  styles: paths.styles.src + '**/*.styl',
+  scripts: paths.scripts.src + '*.js'
+};
+
+// dev variables
+
+var isProduction = true;
+var stylusStyle = 'compressed';
+var sourceMap = false;
+if(gulpUtil.env.dev === true) {
+  stylusStyle = 'expanded';
+  sourceMap = true;
+  isProduction = false;
+}
+
+
+/**
+* Compile stylus files from dev to site staging
+*/
+gulp.task('stylus', function(){
+  return gulp.src(
+    appFiles.styles
+  )
+  .pipe(stylus({
+    onError: browserSync.notify,
+    "use": koutoSwiss()
+  }))
+  .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
+  .pipe(concatCss('app.css'))
+  .pipe(isProduction ? (combineMQ(
+    {log: true})) : gulpUtil.noop())
+  .pipe(isProduction ? (unCss({
+    html: ['http://localhost:3001/']
+  })) : gulpUtil.noop())
+  .pipe(isProduction ? (cleanCss()): gulpUtil.noop())
+  .pipe(gulp.dest('./_site/assets/css'))
+  .pipe(browserSync.stream());
+});
+
+/**
+* Compile pug files from dev to site staging
+*/
+// gulp.task('pug', function(){
+//   return gulp.src(src.pug)
+//   .pipe(pug({
+//     pretty: true
+//   }))
+//   .pipe(gulp.dest(basePaths.dest));
+// });
+
+gulp.task('scripts', function(){
+  gulp.src([
+    basePaths.bower + 'jquery/dist/jquery.js'
+  ])
+  .pipe(concat('app.js'))
+  .pipe(gulp.dest('./_site/assets/js'));
+});
+
+gulp.task('watch', ['stylus', 'scripts'], function() {
+  gulp.watch(appFiles.styles, ["stylus"]);
+  gulp.watch(appFiles.scripts, ["scripts"]);
+  // gulp.watch(src.pug, ['pug']);
+});
+
+gulp.task('php', function(){
+  php.server({base: '_site', port:8010, keepalive: true});
+});
+
+//Copy recursive folder and files from _dev to _site directories
+// use / if tou want to copy all subdirectories
+gulp.task('copy', function(){
+  gulp.src([
+    basePaths.dev + 'assets/fonts/**',
+    basePaths.dev + 'assets/images/**',
+    basePaths.dev +'site/blueprints/**',
+    basePaths.dev + 'site/controllers/**',
+    basePaths.dev + 'site/snippets/**',
+    basePaths.dev + 'site/templates/**',
+    basePaths.dev + 'content/**'
+  ],{
+    "base" : basePaths.dev
+  })
+  .pipe(gulp.dest(basePaths.dest));
+});
+
+
+gulp.task('serve', ['php'], function(){
+  browserSync.init({
+    proxy: '127.0.0.1:8010',
+    port: 8080,
+    open: true,
+    notify: false
+  });
+
+  gulp.watch(appFiles.styles, ['stylus']);
+  gulp.watch("./_site/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['copy', 'serve', 'watch']);
